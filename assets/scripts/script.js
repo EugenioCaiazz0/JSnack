9d8451c3873ae9ac26ab2b48c941e3fd5bb62901
EugenioCaiazz0 <eugeniodanilenko@icloud.com>
2024-03-07 17:38:38 +0100

    end

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -200,4 +200,23 @@ function mergeArrays(element1, element2, element3)
 mergeArrays(arraY1, arraY2, array3);
 
 
+/***************************************/
 
+/* Snack 5 */ 
+
+function betweenAB(arrayElement, numA, numB)
+{
+    if (numA != 0 && numA < numB && numB <= arrayElement.length) 
+    {
+        let resultArray = [];
+        for( let i = numA; i <= numB; i++)
+        {
+            resultArray.push(arrayElement[i]);
+        }
+
+        console.log(resultArray);
+        return(resultArray);
+    }
+}
+
+betweenAB(zucchine, 3, 7); /* usare rest?*/
