f3fca313e3fdaa26624210e6c785af7383ec6f5c
EugenioCaiazz0 <eugeniodanilenko@icloud.com>
2024-03-07 16:44:11 +0100

    Snack 4 done

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -70,78 +70,66 @@ sommaZucchine();
 
 /* JSnack 2 */
 
- const zucchina1 =
-{
-    varietà: `zucchino romanesco`,
-    peso: 500,
-    lunghezza: 17,
-}
+const zucchine2 = [
+    {
+        varietà: `zucchino romanesco`,
+        peso: 500,
+        lunghezza: 17,
+    },
+    {
+        varietà: `zucchino fiorentino`,
+        peso: 350,
+        lunghezza: 12,
+    },
+    {
+        varietà: `zucchina napoletana`,
+        peso: 450,
+        lunghezza: 15,
+    },
+    {
+        varietà: `zucchine tonde`,
+        peso: 450,
+        lunghezza: 12,
+    },
+    {
+        varietà: `zucchine cubiche`,
+        peso: 350,
+        lunghezza: 18,
+    },
+    {
+        varietà: `zucchine non euclidee`,
+        peso: 1,
+        lunghezza: 1,
+    },
+    {
+        varietà: `zucchine zuccherate`,
+        peso: 600,
+        lunghezza: 14,
+    },
+    {
+        varietà: `zucchine salate`,
+        peso: 500,
+        lunghezza: 10,
+    },
+    {
+        varietà: `zucchine senza sapore`,
+        peso: 350,
+        lunghezza: 15,
+    },
+    {
+        varietà: `zucchine mastodontiche`,
+        peso: 10000,
+        lunghezza: 100,
+    }
+]
 
-const zucchina2 =
-{
-    varietà: `zucchino fiorentino`,
-    peso: 350,
-    lunghezza: 12,
-}
 
-const zucchina3 =
-{
-    varietà: `zucchina napoletana`,
-    peso: 450,
-    lunghezza: 15,
-}
+const ages = [32, 33, 16, 40];
+const result = ages.filter(checkAdult);
 
-const zucchina4 =
-{
-    varietà: `zucchine tonde`,
-    peso: 450,
-    lunghezza: 12,
+function checkAdult(age) {
+  return age >= 18;
 }
-
-const zucchina5 =
-{
-    varietà: `zucchine cubiche`,
-    peso: 350,
-    lunghezza: 18,
-}
-
-const zucchina6 =
-{
-    varietà: `zucchine non euclidee`,
-    peso: 1,
-    lunghezza: 1,
-}
-
-const zucchina7 =
-{
-    varietà: `zucchine zuccherate`,
-    peso: 600,
-    lunghezza: 14,
-}
-
-const zucchina8 =
-{
-    varietà: `zucchine salate`,
-    peso: 500,
-    lunghezza: 10,
-}
-
-const zucchina9 =
-{
-    varietà: `zucchine senza sapore`,
-    peso: 350,
-    lunghezza: 15,
-}
-
-const zucchina10 =
-{
-    varietà: `zucchine mastodontiche`,
-    peso: 10000,
-    lunghezza: 100,
-}
-
-const arrayOver15 = [];
-const arrayUnder15 = [];
 
 
 
@@ -180,4 +168,32 @@ stringInverter3();
 /********************************/
 
 /* Snack 4 */
+
+let arraY1 = [`word1`, `word2`, `word3`, `word4`, `word5`];
+let arraY2 = [`word6`, `word7`, `word8`, `word9`, `word10`];
+let array3 = [];
+
+function mergeArrays(element1, element2, element3) 
+{
+    if(element1.length == element2.length) 
+    {
+        for(i = 0; i < element1.length; i++) 
+        {
+            arrayElement1 = element1[i];
+            arrayElement2 = element2[i];
+
+            element3.push(arrayElement1);
+            element3.push(arrayElement2);
+        }
+        console.log(element3);
+        return element3;
+    } else 
+    {
+        console.log(`merge non applicabile con funzioni di diversa lunghezza`);
+    }
+}
+
+mergeArrays(arraY1, arraY2, array3);
+
+
 
